1c95d0f11fbe41cba1384cd19dd9ddf7d8a7d6a9
maudel <maudelaquintana@gmail.com>
2018-11-24 00:49:34 -0400

    pedidos list

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 
 import 'package:cakes_catalog/my_home_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cakes_catalog/pedidos_list.dart';
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
       primarySwatch: Colors.cyan
     ),
-    home: MyHomePage(),
+    home: PedidosList(),
   ));
 }
